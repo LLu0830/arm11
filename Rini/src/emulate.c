0c95f6c9369e22103ec1ba72e02a7e4134f1321d
rbanerjee20 <rinibanerjee@dyn3161-191.wlan.ic.ac.uk>
2019-05-23 17:56:44 +0100

    Finished binary reader

--- Rini/src/emulate.c
+++ Rini/src/emulate.c
@@ -2,13 +2,14 @@
 #include <stdio.h>
 #include <string.h>
 
+typedef uint32_t Word;
+
 int main(int argc, char **argv) {
     FILE *fp;
-    unsigned char c;
+    Word *mem = calloc(16384, sizeof(Word));
     fp = fopen(argv[0], "rb");
-
-    while (!(feof(fp))) {
-        c = fread();
+    while (!feof(fp)) {
+        fread(mem, sizeof(Word), 1, fp);
     }
     return EXIT_SUCCESS;
 }
