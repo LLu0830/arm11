b3ee059046808b76d724af5c458e7368481d0411
rbanerjee20 <rinibanerjee@dyn3170-125.wlan.ic.ac.uk>
2019-05-24 15:37:22 +0100

    Updated 24-05

--- Rini/src/emulate.c
+++ Rini/src/emulate.c
@@ -2,14 +2,13 @@
 #include <stdio.h>
 #include <string.h>
 
-typedef uint32_t Word;
 
 int main(int argc, char **argv) {
     FILE *fp;
-    Word *mem = calloc(16384, sizeof(Word));
+    uint32_t *mem = calloc(16384, sizeof(uint32_t));
     fp = fopen(argv[0], "rb");
     while (!feof(fp)) {
-        fread(mem, sizeof(Word), 1, fp);
+        fread(mem, sizeof(uint32_t), 1, fp);
     }
     return EXIT_SUCCESS;
 }
