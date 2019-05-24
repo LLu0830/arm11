1e8c52af3edc4c79097b4ac0c556f05d02a96482
unknown <mw6118@ic.ac.uk>
2019-05-24 16:18:09 +0100

    add some Enum

---
+++ src/instruction.h
@@ -0,0 +1,38 @@
+//
+// Created by MANSHU WANG on 5/24/2019.
+//
+
+#ifndef C_GROUP_31_INSTRUCTION_H
+#define C_GROUP_31_INSTRUCTION_H
+
+#endif //C_GROUP_31_INSTRUCTION_H
+enum OpCode{
+    AND,
+    EOR,
+    SUB,
+    RSB,
+    ADD,
+    TST,
+    TEQ,
+    CMP,
+    ORR,
+    MOV
+};
+
+
+enum InstructionType{
+    DataProcessing,
+    Multiply,
+    SingleDataTransfer,
+    Branch
+};
+
+enum Cond{
+    EQ,
+    NE,
+    GE,
+    LT,
+    GT,
+    LE,
+    AL
+};
---
+++ src/multiplyInstruction/Multiply.c
@@ -0,0 +1,6 @@
+//
+// Created by MANSHU WANG on 5/24/2019.
+//
+
+
+
