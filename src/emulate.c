bd9187f1a7087e723244fb48848a89fc193c1663
rbanerjee20 <rinibanerjee@rinis-mbp-1575.home>
2019-05-26 15:24:28 +0100

    Added binary loader function

--- src/emulate.c
+++ src/emulate.c
@@ -7,17 +7,9 @@
 #include "registers.h"
 #include <math.h>
 
-//keep trying
-
 int main(int argc, char **argv) {
     struct registers ARM_registers;
-    int size32 = sizeof(uint32_t);
-    ARM_registers.reg = calloc(17, size32);
-    uint32_t *mem = calloc((pow(2, 16) / size32), size32);
-    FILE *fp;
-    fp = fopen(argv[0], "rb");
-    while (!feof(fp)) {
-        fread(mem, size32, 1, fp);
-    }
+    ARM_registers.reg = calloc(17, sizeof(uint32_t));
+    uint32_t *mem = binaryFileLoader(argv);
     return EXIT_SUCCESS;
 }
