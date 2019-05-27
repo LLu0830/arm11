039f4b8030e80838a38ff3981c396df9ded0134f
LizeLu <lize.lu18@imperial.ac.uk>
2019-05-27 16:32:39 +0100

    Update registers.h, added arrays and PC int,not sure about that tho

--- src/registers.h
+++ src/registers.h
@@ -2,18 +2,19 @@
 // Created by Rini Banerjee on 2019-05-25.
 //
 
-
 #include <stdio.h>
-#include "DefinedTypes.h"
 
 #ifndef SRC_REGISTERS_H
 #define SRC_REGISTERS_H
 
 struct registers {
-//    int regs[17];
-//    uint32_t vals[17];
     uint32_t *reg;
+    //uint32_t reg[17];
 
+    int GP[32];
+    int PC[32];
+    int CPSR[32];
+    int PC_int = *(reg + 15);
 };
 
 #endif //SRC_REGISTERS_H
