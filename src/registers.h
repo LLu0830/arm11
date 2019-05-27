bc1613487422787071b77ff94c29d5c631c3150a
rbanerjee20 <rinibanerjee@rinis-mbp-1575.home>
2019-05-27 11:21:31 +0100

    h

---
+++ src/registers.h
@@ -0,0 +1,19 @@
+//
+// Created by Rini Banerjee on 2019-05-25.
+//
+
+
+#include <stdio.h>
+#include "DefinedTypes.h"
+
+#ifndef SRC_REGISTERS_H
+#define SRC_REGISTERS_H
+
+struct registers {
+//    int regs[17];
+//    uint32_t vals[17];
+    uint32_t *reg;
+
+};
+
+#endif //SRC_REGISTERS_H
