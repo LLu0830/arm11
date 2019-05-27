f10f162796966c6fbf199bff366088bafcfd8e17
rbanerjee20 <rinibanerjee@rinis-mbp-1575.home>
2019-05-27 11:12:31 +0100

    committing

--- src/registers.h
+++ src/registers.h
@@ -2,23 +2,15 @@
 // Created by Rini Banerjee on 2019-05-25.
 //
 
-<<<<<<< HEAD
-=======
+
 #include <stdio.h>
 #include "DefinedTypes.h"
->>>>>>> dec3e21c2400928967b863e88f33ca1db3888950
+
 #ifndef SRC_REGISTERS_H
 #define SRC_REGISTERS_H
 
 struct registers {
-<<<<<<< HEAD
-    int regs[17];
-    uint32_t vals[17];
-=======
     uint32_t *reg;
-
-    
->>>>>>> dec3e21c2400928967b863e88f33ca1db3888950
 };
 
 #endif //SRC_REGISTERS_H
