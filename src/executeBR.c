4da1031318cdd696df3bf74cef20eb65bdd8f17d
Manshu Wang <mw6118@edge20.doc.ic.ac.uk>
2019-05-28 17:03:12 +0100

    samll change in branch

--- src/executeBR.c
+++ src/executeBR.c
@@ -11,6 +11,7 @@
 #include "memory.h"
 #include "executeBR.h"
 #include "utility.h"
+#include "DefinedTypes.h"
 
 void twos_complement_extend(int offset) {
     const int bits = 24;
@@ -22,8 +23,8 @@ void twos_complement_extend(int offset) {
 void executeBR(struct registers ARM11_registers, uint32_t b) {
     int cond = get_n_bits(b, 28, 4);
     int offset = get_n_bits(b, 0, 23);
-    _Bool valid_cond = (cond == 0000 || cond == 0001 || cond == 1010 || cond == 1011 || cond == 1100 || cond == 1101 ||
-                        cond == 1110);
+    _Bool valid_cond = (cond == EQ || cond == NE || cond == GE || cond == LT || cond == GT || cond == LE ||
+                        cond == AL);
     if (valid_cond) {
         //2's complement extend
         twos_complement_extend(offset);
