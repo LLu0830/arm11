f38a1c1211d5c26163a461c91132002215eb72b9
ll7818 <ll7818@ic.ac.uk>
2019-05-29 21:37:17 +0100

    pipeline

--- src/pipeline.c
+++ src/pipeline.c
@@ -14,32 +14,30 @@
 #include "DefinedTypes.h"
 
 
+#define PCPosition 15;
 
 //add pipeline
 
-void pipeline(struct stateOfMachine ARM11,struct pipes pipe) {
+void pipeline(struct stateOfMachine ARM11, struct pipes pipe) {
 
     //execute
     if (pipe.decoded.InstructionType != NUL) {
         execute(pipe.decoded, ARM11);
     }
-
-
     //decode
+    //pipe.decoded=(uint32_t) 0;
     if (pipe.has_fetched) {
         decode(ARM11, pipe.fetched, pipe.decoded.conditionType);
     }
-
     //fetch
     //where does this address come from
     if (pipe.decoded.conditionType != HLT) {
-        ARM11.registers.fetched = fetch(ARM11);
+        pipe.fetched = fetch(ARM11);
         pipe.has_fetched = true;
     } else {
         pipe.has_fetched = false;
     }
-
     //PC
-    ARM11.registers[15] += 4;
+    ARM11.registers[PCPosition] += 4;
 }
 
