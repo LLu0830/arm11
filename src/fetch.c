bd5724a4393e1cc9bda2c7e0eeeb7d1d388ebfbf
ll7818 <ll7818@ic.ac.uk>
2019-05-29 01:01:04 +0100

    fix fetch.h

--- src/fetch.c
+++ src/fetch.c
@@ -21,9 +21,9 @@
 // also a _Bool tells if the reg contains fetched instruction, called "has_fetched"
 
 uint32_t fetch(struct stateOfMachine ARM11) {
-    uint8_t mem=ARM11.mem;
+    uint8_t *mem = ARM11.mem;
     uint32_t f = 0;
-    uint32_t PC=(int) ARM11.registers[15];
+    uint32_t PC = (int) ARM11.registers[15];
     for (int i = 0; i < 4; i++) {
         f |= ((uint32_t) * (mem + PC + i) << (i * 8));
     }
