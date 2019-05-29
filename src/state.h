caf83cfe58ab0d129d5615ab5c585e42095e25be
ll7818 <ll7818@ic.ac.uk>
2019-05-29 14:24:32 +0100

    fix state

--- src/state.h
+++ src/state.h
@@ -13,23 +13,10 @@
 #define CPSRPosition 16
 #define PCPosition 15
 
-struct registers {
-    uint32_t registers[17];
-    uint32_t fetched[17];
-    InstructionType decoded[17];
-    _Bool has_fetched;
-};
-
 struct stateOfMachine {
-    struct registers registers;
-    uint32_t *mem;
-//    (Rini) Katarina was saying that this needs to be a uint8_t?
+    uint32_t registers[17];
+    uint8_t *mem;
 };
 
-
-//struct stateOfMachine {
-//    uint32_t registers[17];
-//    uint8_t *mem;
-//};
 
 #endif //C_GROUP_31_STATE_H
