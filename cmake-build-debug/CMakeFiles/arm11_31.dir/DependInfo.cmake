50a090bb6ec33aabf02a7be7f1c4dcc1183cb160
rbanerjee20 <rinibanerjee@dyn3168-233.wlan.ic.ac.uk>
2019-05-31 15:32:06 +0100

    Minor tweaks

--- cmake-build-debug/CMakeFiles/arm11_31.dir/DependInfo.cmake
+++ cmake-build-debug/CMakeFiles/arm11_31.dir/DependInfo.cmake
@@ -4,21 +4,21 @@ set(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_C
-  "/homes/mw6118/CLionProjects/arm11_31/src/assemble.c" "/homes/mw6118/CLionProjects/arm11_31/cmake-build-debug/CMakeFiles/arm11_31.dir/src/assemble.c.o"
-  "/homes/mw6118/CLionProjects/arm11_31/src/part1_emulator/decode/decode.c" "/homes/mw6118/CLionProjects/arm11_31/cmake-build-debug/CMakeFiles/arm11_31.dir/src/part1_emulator/decode/decode.c.o"
-  "/homes/mw6118/CLionProjects/arm11_31/src/part1_emulator/emulate.c" "/homes/mw6118/CLionProjects/arm11_31/cmake-build-debug/CMakeFiles/arm11_31.dir/src/part1_emulator/emulate.c.o"
-  "/homes/mw6118/CLionProjects/arm11_31/src/part1_emulator/emulate/binaryFileLoader.c" "/homes/mw6118/CLionProjects/arm11_31/cmake-build-debug/CMakeFiles/arm11_31.dir/src/part1_emulator/emulate/binaryFileLoader.c.o"
-  "/homes/mw6118/CLionProjects/arm11_31/src/part1_emulator/emulate/pipeline.c" "/homes/mw6118/CLionProjects/arm11_31/cmake-build-debug/CMakeFiles/arm11_31.dir/src/part1_emulator/emulate/pipeline.c.o"
-  "/homes/mw6118/CLionProjects/arm11_31/src/part1_emulator/emulate/printOutput.c" "/homes/mw6118/CLionProjects/arm11_31/cmake-build-debug/CMakeFiles/arm11_31.dir/src/part1_emulator/emulate/printOutput.c.o"
-  "/homes/mw6118/CLionProjects/arm11_31/src/part1_emulator/emulator_utility/utility.c" "/homes/mw6118/CLionProjects/arm11_31/cmake-build-debug/CMakeFiles/arm11_31.dir/src/part1_emulator/emulator_utility/utility.c.o"
-  "/homes/mw6118/CLionProjects/arm11_31/src/part1_emulator/execute/execute.c" "/homes/mw6118/CLionProjects/arm11_31/cmake-build-debug/CMakeFiles/arm11_31.dir/src/part1_emulator/execute/execute.c.o"
-  "/homes/mw6118/CLionProjects/arm11_31/src/part1_emulator/execute/executeBR.c" "/homes/mw6118/CLionProjects/arm11_31/cmake-build-debug/CMakeFiles/arm11_31.dir/src/part1_emulator/execute/executeBR.c.o"
-  "/homes/mw6118/CLionProjects/arm11_31/src/part1_emulator/execute/executeDP.c" "/homes/mw6118/CLionProjects/arm11_31/cmake-build-debug/CMakeFiles/arm11_31.dir/src/part1_emulator/execute/executeDP.c.o"
-  "/homes/mw6118/CLionProjects/arm11_31/src/part1_emulator/execute/executeMUL.c" "/homes/mw6118/CLionProjects/arm11_31/cmake-build-debug/CMakeFiles/arm11_31.dir/src/part1_emulator/execute/executeMUL.c.o"
-  "/homes/mw6118/CLionProjects/arm11_31/src/part1_emulator/execute/executeSDT.c" "/homes/mw6118/CLionProjects/arm11_31/cmake-build-debug/CMakeFiles/arm11_31.dir/src/part1_emulator/execute/executeSDT.c.o"
-  "/homes/mw6118/CLionProjects/arm11_31/src/part1_emulator/fetch/fetch.c" "/homes/mw6118/CLionProjects/arm11_31/cmake-build-debug/CMakeFiles/arm11_31.dir/src/part1_emulator/fetch/fetch.c.o"
+  "/Users/rinibanerjee/Desktop/C_Group_31/src/assemble.c" "/Users/rinibanerjee/Desktop/C_Group_31/cmake-build-debug/CMakeFiles/arm11_31.dir/src/assemble.c.o"
+  "/Users/rinibanerjee/Desktop/C_Group_31/src/part1_emulator/decode/decode.c" "/Users/rinibanerjee/Desktop/C_Group_31/cmake-build-debug/CMakeFiles/arm11_31.dir/src/part1_emulator/decode/decode.c.o"
+  "/Users/rinibanerjee/Desktop/C_Group_31/src/part1_emulator/emulate.c" "/Users/rinibanerjee/Desktop/C_Group_31/cmake-build-debug/CMakeFiles/arm11_31.dir/src/part1_emulator/emulate.c.o"
+  "/Users/rinibanerjee/Desktop/C_Group_31/src/part1_emulator/emulate/binaryFileLoader.c" "/Users/rinibanerjee/Desktop/C_Group_31/cmake-build-debug/CMakeFiles/arm11_31.dir/src/part1_emulator/emulate/binaryFileLoader.c.o"
+  "/Users/rinibanerjee/Desktop/C_Group_31/src/part1_emulator/emulate/pipeline.c" "/Users/rinibanerjee/Desktop/C_Group_31/cmake-build-debug/CMakeFiles/arm11_31.dir/src/part1_emulator/emulate/pipeline.c.o"
+  "/Users/rinibanerjee/Desktop/C_Group_31/src/part1_emulator/emulate/printOutput.c" "/Users/rinibanerjee/Desktop/C_Group_31/cmake-build-debug/CMakeFiles/arm11_31.dir/src/part1_emulator/emulate/printOutput.c.o"
+  "/Users/rinibanerjee/Desktop/C_Group_31/src/part1_emulator/emulator_utility/utility.c" "/Users/rinibanerjee/Desktop/C_Group_31/cmake-build-debug/CMakeFiles/arm11_31.dir/src/part1_emulator/emulator_utility/utility.c.o"
+  "/Users/rinibanerjee/Desktop/C_Group_31/src/part1_emulator/execute/execute.c" "/Users/rinibanerjee/Desktop/C_Group_31/cmake-build-debug/CMakeFiles/arm11_31.dir/src/part1_emulator/execute/execute.c.o"
+  "/Users/rinibanerjee/Desktop/C_Group_31/src/part1_emulator/execute/executeBR.c" "/Users/rinibanerjee/Desktop/C_Group_31/cmake-build-debug/CMakeFiles/arm11_31.dir/src/part1_emulator/execute/executeBR.c.o"
+  "/Users/rinibanerjee/Desktop/C_Group_31/src/part1_emulator/execute/executeDP.c" "/Users/rinibanerjee/Desktop/C_Group_31/cmake-build-debug/CMakeFiles/arm11_31.dir/src/part1_emulator/execute/executeDP.c.o"
+  "/Users/rinibanerjee/Desktop/C_Group_31/src/part1_emulator/execute/executeMUL.c" "/Users/rinibanerjee/Desktop/C_Group_31/cmake-build-debug/CMakeFiles/arm11_31.dir/src/part1_emulator/execute/executeMUL.c.o"
+  "/Users/rinibanerjee/Desktop/C_Group_31/src/part1_emulator/execute/executeSDT.c" "/Users/rinibanerjee/Desktop/C_Group_31/cmake-build-debug/CMakeFiles/arm11_31.dir/src/part1_emulator/execute/executeSDT.c.o"
+  "/Users/rinibanerjee/Desktop/C_Group_31/src/part1_emulator/fetch/fetch.c" "/Users/rinibanerjee/Desktop/C_Group_31/cmake-build-debug/CMakeFiles/arm11_31.dir/src/part1_emulator/fetch/fetch.c.o"
   )
-set(CMAKE_C_COMPILER_ID "GNU")
+set(CMAKE_C_COMPILER_ID "AppleClang")
 
 # The include file search paths:
 set(CMAKE_C_TARGET_INCLUDE_PATH
