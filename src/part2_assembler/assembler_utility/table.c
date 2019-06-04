26b757d1f3e980aaec3cc5ece1344fd7ca4cb653
ll7818 <ll7818@ic.ac.uk>
2019-06-04 13:24:41 +0100

    utility

--- src/part2_assembler/assembler_utility/table.c
+++ src/part2_assembler/assembler_utility/table.c
@@ -1,4 +1,62 @@
+
+
 //
-// Created by ASUS on 2019/6/4.
+// Created by Lize Lu on 2019/6/4.
 //
+
+#ifndef SRC_TABLE_C
+#define SRC_TABLE_C
+#define SIZE 20
+
+#include "../assembler_utility/table.h"
+
+typedef struct label_address {
+    *label label;
+    *address address;
+} label_address;
+
+struct label_address *labelArray[SIZE];
+struct label_address *aPair;
+
+int hashCode(
+        label *label
+) {
+    return label % SIZE;
+}
+
+void insert_label(
+        label *label, address *address) {
+    struct label_address *aPair = (struct label_address *) malloc(sizeof(struct label_address *));
+    *aPair->label = label;
+    *aPair->address = address;
+    int hashIndex = hashCode(label);
+    while (labelArray[hashIndex])  != NULL && hashArray[hashIndex]->key != -1) {
+//go to next cell
+        ++hashIndex;
+
+//wrap around the table
+        hashIndex %= SIZE;
+    }
+    labelArray[hashIndex] = aPair;
+}
+
+//static instruction *createNewTable(uint16_t numOfInstru)
+//
+//        string_arrays_t *make_string_arrays(void) {
+//    string_arrays_t *string_arrays = malloc(sizeof(string_arrays_t));
+//    if (!string_arrays) {
+//        perror("Unable to allocate memory for string arrays");
+//        exit(EXIT_FAILURE);
+//    }
+//    string_arrays->max_elements = INITIAL_ARRAY_SIZE;
+//    string_arrays->arrays = malloc(string_arrays->max_elements
+//                                   * sizeof(string_array_t *));
+//    if (!string_arrays->arrays) {
+//        perror("Unable to allocate memory for string arrays");
+//        exit(EXIT_FAILURE);
+//    }
+//    string_arrays->size = 0;
+//    return string_arrays;
+//}
+
 
--- src/part2_assembler/assembler_utility/table.h
+++ src/part2_assembler/assembler_utility/table.h
@@ -2,11 +2,74 @@
 // Created by Katarina Kulkova on 02.06.2019.
 //
 
+
 #ifndef SRC_TABLE_H
 #define SRC_TABLE_H
+
+#include <stdint.h>
+#include "../emulator_utility/DefinedTypes.h"
+#include "../emulator_utility/instruction.h"
+//#include "assembler_utility.h"
+
 
 //find out how to make hash map/ hash table?? in C an implement it here
 
 //add the two structs
+typedef struct token{
+    string name;
+    uint32_t code;
+} token;
+
+typedef struct instruction{
+    token mnemonic;
+    token operand1;
+    token operand2;
+    token operand3;
+    token operand4;
+    InstructionType type;
+    uint32_t encoded;
+    int counter;//counter in readInstruction
+} instruction;
+
+typedef char *label;
+typedef int *address;
+
+typedef struct label_address{
+    *label label;
+    *address address;
+}label_address;
+
+
+
+
+
+//typedef struct {
+//    /** Array of strings. */
+//    char **array;
+//    /** The number of strings in the array. */
+//    uint16_t size;
+//} string_array;
+//
+//typedef struct {
+//    /** The label string, with maximum length of MAX_LABEL_LENGTH */
+//    char label[MAX_LABEL_LENGTH + 1];
+//    /** The address of this label */
+//    address address;
+//} table_row;
+//
+//typedef struct {
+//    /** Maximum number of rows in symbol table. */
+//    uint16_t max_size;
+//    /** Number of rows in symbol table. */
+//    uint16_t size;
+//    /** An array of symbol table rows. */
+//    table_row *rows;
+//} table;
+//
+//table *generate_symbol_table(string_arrays *tokens);
+//address get_address(table_t *table, char *label);
+//void free_table(symbol_table_t *table);
+
 
 #endif //SRC_TABLE_H
+
