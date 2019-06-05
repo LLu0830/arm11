33214a8be1774cd915c5fe5dd9cd891f35e0e4be
ll7818 <ll7818@ic.ac.uk>
2019-06-05 22:46:45 +0100

    finished readLabels.c,table.c,encodeBR.c

--- src/part2_assembler/assembler_utility/table.h
+++ src/part2_assembler/assembler_utility/table.h
@@ -60,7 +60,7 @@ typedef struct {
     token arg3;
     token arg4;
     InstructionType type;
-    Mnemonic *operationType;
+    Mnemonic operationType;
     uint32_t encoded;
 
     address target_address;
@@ -91,8 +91,11 @@ typedef struct {
 
 //do these have to be static?
 label_address_list initialize_list();
+
 label_address *initialize_pair();
+
 void insert_pair(label_address pair, label_address_list *list);
+
 label_address *lookup_pair(label *label);
 //struct label_address_list *allocList(void);
 
