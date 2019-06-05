d6e5dab8a8b22de92f0abc449ab0e9e68170d8f8
kk6418 <kk6418@DESKTOP-9MOO913.localdomain>
2019-06-05 19:51:26 +0100

    tokenizer, read instruction and some utility functions

--- src/part2_assembler/tokenizer/tokenizer.c
+++ src/part2_assembler/tokenizer/tokenizer.c
@@ -6,13 +6,9 @@
 #include <stdint.h>
 #include <stdio.h>
 #include "tokenizer.h"
-#include "../assembler_utility/table.h"
+#include "../../part1_emulator/emulator_utility/DefinedTypes.h"
+#include "../assembler_utility/assembler_utility.h"
 
-char *copy_string(char *string){
-    char *result = malloc(strlen(string) + 1);
-    strcpy(result, string);
-    return result;
-}
 
 void tokenizer(char *instruction, assembler_instruction *parsed) {
 
@@ -23,5 +19,5 @@ void tokenizer(char *instruction, assembler_instruction *parsed) {
     parsed->arg3 = copy_string(strtok_r(NULL, ", ", &ptr));
     parsed->arg4 = copy_string(strtok_r(NULL, ", ", &ptr));
 
-    parsed->type =;
+    parsed->type = get_instruction_type(parsed->mnemonic);
 }
