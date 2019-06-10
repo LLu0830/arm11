7f688250d127539aeef0e1ebe9cec3bb8c884ba3
ll7818 <ll7818@ic.ac.uk>
2019-06-10 16:07:04 +0100

    fixed unmatched type in lookup_address in table.c

--- src/part2_assembler/encode/encodeBR.c
+++ src/part2_assembler/encode/encodeBR.c
@@ -6,7 +6,7 @@
 #include <assembler_utility.h>
 #include "encodeBR.h"
 #include "../assembler_utility/table.h"
-
+#include "table.h"
 
 uint32_t getCond(char *condition) {
     uint32_t result = 1110;
@@ -35,10 +35,12 @@ uint32_t getCond(char *condition) {
     return result;
 }
 
+
+
 void encodeBR(assembler_instruction *instruction) {
     address target;
     if (isLabel(instruction->arg1)) {
-        target = lookup_pair(instruction->arg1);
+        target=lookup_address(instruction->arg1);
     } else {
         target = instruction->arg1;
     }
