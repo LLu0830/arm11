8f9074b3829b2039f65063659fa19c6f4ddc5e78
rbanerjee20 <rinibanerjee@dyn3163-180.wlan.ic.ac.uk>
2019-06-13 17:40:52 +0100

    ..

--- src/part1_emulator/execute/executeSDT.c
+++ src/part1_emulator/execute/executeSDT.c
@@ -112,7 +112,8 @@ void executeSDT(Instruction instruction, struct stateOfMachine *state) {
         }
 
     } else if (address >= GPIO_0_TO_9 && address <= CLEAR_ADDRESS) {
-        state->registers[rd] = address;
+        if (lFlag) {
+            state->registers[rd] = address;
     } else {
         printf("Error: Out of bounds memory access at address 0x%08x\n", address);
     }
