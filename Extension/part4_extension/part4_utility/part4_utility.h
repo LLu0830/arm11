871266d16ee44dcd5a6ef4d554aef681ac5a847f
Lize Lu <lize.lu18@imperial.ac.uk>
2019-06-19 20:54:23 +0100

    minor changes on part4_utility.h

--- Extension/part4_extension/part4_utility/part4_utility.h
+++ Extension/part4_extension/part4_utility/part4_utility.h
@@ -9,7 +9,6 @@
 #include <stdbool.h>
 
 
-int main();
 
 void strrev(char *s);
 
@@ -19,13 +18,11 @@ bool isBin(char *number);
 
 int bin2dec(char *bin);
 
-
-char *dec2bin(int n);
-
 int operate_arith(int n1, int n2, char *operator);
 
 char *int_to_str(int n);
 
+char *dec2bin(int n);
 
 #endif //ARM11_31_PART4_UTILITY_H
 
